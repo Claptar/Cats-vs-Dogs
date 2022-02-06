54a500ed99b5d09c82cc6e21479f357528325fce
Claptar <binkevich.as@phystech.edu>
2022-02-06 15:28:32 +0300

    Small fixes

--- models/fc_model.py
+++ models/fc_model.py
@@ -2,15 +2,16 @@ from torch import nn, optim
 
 
 class SimpleFC(nn.Module):
-    def __init__(self, in_features, dropout=0.5):
+    def __init__(self, in_features, dropout=0.5, num_classes=2):
         super().__init__()
         self.model = nn.Sequential(
+            nn.Flatten(),
             nn.Linear(in_features, 256),
             nn.ReLU(),
             nn.Linear(256, 256),
             nn.LeakyReLU(0.1),
             nn.Dropout(p=dropout),
-            nn.Linear(256, 1)
+            nn.Linear(256, num_classes)
         )
 
     def forward(self, input):
