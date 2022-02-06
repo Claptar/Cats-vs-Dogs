1e87fdfae4429a1aff0d4e92cab3fe60a75e9f46
Claptar <binkevich.as@phystech.edu>
2022-02-06 17:09:33 +0300

    Added train_loop func

--- train/train.py
+++ train/train.py
@@ -1,6 +1,8 @@
 import torch
 from torch import optim
 from tqdm import tqdm
+import wandb
+import numpy as np
 
 
 def train_epoch(model, criterion, optimizer, dataloader):
@@ -9,9 +11,29 @@ def train_epoch(model, criterion, optimizer, dataloader):
         # making predictions and calculating loss
         predictions = model(data)
         loss = criterion(predictions, target)
+        # logging loss
+        loss_log.append(loss.item())
+        wandb.log({'batch_loss': loss.item()})
         # making backprop
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+    return loss_log
+
+
+def train_loop(model, criterion, optimizer, train_loader, test_loader, n_epoch):
+    for epoch in range(n_epoch):
+        loss_log = train_epoch(model, criterion, optimizer, train_loader)
+        train_accuracy = compute_accuracy(model, train_loader)
+        test_accuracy = compute_accuracy(model, test_loader)
+        wandb.log({
+            'mean_epoch_loss': np.array(loss_log).mean(),
+            'train_accuracy': train_accuracy,
+            'test_accuracy': test_accuracy
+        })
+
+
+def compute_accuracy(model, dataloader):
+    return 'Not implemented yet'
 
 
